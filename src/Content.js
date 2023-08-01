2cdc9d85a67b3d961d760365a7b30a27cc158c39
Muthulakshmy <muthulakshmy14072002@gmail.com>
2023-08-01 11:10:07 +0530

    rtyu

---
+++ src/Content.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Content = () => {
+    return (
+        <div className='content'>
+            <h1>Opening Party!</h1>
+            <p>written by yoshi</p>
+            <h1>Welcome to the New Blog</h1>
+            <p>written by mario</p>
+          
+        </div>
+    )
+}
+
+
+
+export default Content
---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+        <h1 className='title'>The Dojo Blog</h1>
+        <div className='btn'>
+        <button className='home'>Home</button>
+        <button className='newblog'>New Blog</button>
+        </div>
+        
+        </div>
+        
+    )
+}
+
+export default Navbar
